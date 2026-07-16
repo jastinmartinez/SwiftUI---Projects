e6b5fb19a5ffc6263469039edb28190feb4545c4
Jastin Martinez <martinez.jastin@hotmail.com>
2026-07-15 20:01:19 -0400

    refactor: inline eligibility notice model

--- Crescendo/Crescendo/Components/PlaybackEligibilityNoticeView.swift
+++ Crescendo/Crescendo/Components/PlaybackEligibilityNoticeView.swift
@@ -15,3 +15,16 @@ struct PlaybackEligibilityNoticeView: View {
         }
     }
 }
+
+extension PlaybackEligibilityNoticeView {
+    /// The immutable presentation contract for music playback eligibility messaging.
+    struct Model: Equatable {
+        let presentation: Presentation
+
+        enum Presentation: Equatable {
+            case hidden
+            case subscriptionRequired
+            case availabilityUnknown
+        }
+    }
+}
